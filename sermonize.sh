d74168e7784a337990bdd695a6f050c7659e82d0
Keith Walbolt <keith.walbolt@gmail.com>
2013-09-06 13:26:30 -0400

    initial commit

---
+++ sermonize.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+textfile=$1
+sermon=`basename "$textfile" | sed 's/\.txt//'`
+
+# runs pandoc with the xelatex engine using a latex template on an arbitrary multimarkdown file and then opens the pdf that's created with the same name as the txt file (sans txt suffix).
+
+pandoc --latex-engine=xelatex --template="$HOME/.pandoc/sermon.latex" -f markdown_mmd -o "$sermon.pdf" "$textfile"; open "$sermon.pdf"
