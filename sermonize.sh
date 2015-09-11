cda3046eacf954362b3aa0923e6ebe7c74a4ff53
Keith Walbolt <keith.walbolt@gmail.com>
2015-09-11 14:28:36 -0400

    updated script to upload text to pelican static blog

--- sermonize.sh
+++ sermonize.sh
@@ -6,3 +6,7 @@ sermon=`basename "$textfile" | sed 's/\.txt//'`
 # runs pandoc with the xelatex engine using a latex template on an arbitrary multimarkdown file and then opens the pdf that's created with the same name as the txt file (sans txt suffix).
 
 pandoc --latex-engine=xelatex --template="$HOME/.pandoc/sermon.latex" -f markdown_mmd -o "$sermon.pdf" "$textfile"; open "$sermon.pdf"
+
+# prepare sermon for a pelican static blog and then upload it to the server
+cp "$textfile" "$HOME/preaching/content/$sermon.md"
+cd "$HOME/preaching"; make rsync_upload
